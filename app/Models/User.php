60838d9ba682ce764ff88298caa4bf0e81de7006
shohorab-shanto <shohorabshanto@gmail.com>
2025-12-02 14:33:57 +0600

    user rating for teachers

--- app/Models/User.php
+++ app/Models/User.php
@@ -59,4 +59,35 @@ class User extends Authenticatable {
     public function preliminaryAnswer() {
         return $this->hasMany(PreliminaryAnswer::class, 'user_id', 'id');
     }
+
+    // Teacher reviews (reviews received by this teacher)
+    public function teacherReviews() {
+        return $this->hasMany(WrittenAnswerReview::class, 'teacher_id', 'id');
+    }
+
+    // Student reviews (reviews given by this student)
+    public function studentReviews() {
+        return $this->hasMany(WrittenAnswerReview::class, 'user_id', 'id');
+    }
+
+    // Get average rating for teacher
+    public function getAverageRatingAttribute() {
+        if ($this->type !== 'teacher') {
+            return null;
+        }
+        
+        return $this->teacherReviews()->avg('rating') ?? 0;
+    }
+
+    // Get total reviews count for teacher
+    public function getTotalReviewsAttribute() {
+        if ($this->type !== 'teacher') {
+            return 0;
+        }
+        
+        return $this->teacherReviews()->count();
+    }
+
+    // Append these attributes to JSON/Array output
+    protected $appends = [];
 }
